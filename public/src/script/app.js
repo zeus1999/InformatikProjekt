fec04ad37758bfee85b8394110cdec8ce3deb11d
AMAIISDOM\tobias.jung <jung-tobias@outlook.de>
2018-10-19 22:21:11 +0200

    lang saver, translation core

--- public/src/script/app.js
+++ public/src/script/app.js
@@ -1,6 +1,7 @@
-var app = angular.module("projekt", ["ngMaterial", "ngRoute", "pascalprecht.translate"]);
+var app = angular.module("projekt", ["ngMaterial", "ngCookies", "ngRoute", "pascalprecht.translate"]);
 
-app.run(function($rootScope, $window, $location) {
+
+app.run(function($rootScope, $window, $location, $translate, $cookies){
     $rootScope.open = function(url){
         $window.location.href = url;
     }
@@ -8,21 +9,34 @@ app.run(function($rootScope, $window, $location) {
     $rootScope.route = function(url){
         $location.path(url);
     }
+
+
+});
+
+app.controller("langCtrl", function($scope, $cookies, $translate){
+    $scope.language = $cookies.get("lang") || "de_DE";
+
+    $scope.$watch("language", function(newValue, oldValue){
+        $translate.use($scope.language);
+        $cookies.put("lang", $scope.language);
+    });
+
+    
 });
 
 app.config(function($mdThemingProvider, $routeProvider, $locationProvider, $translateProvider){
 
-    $translateProvider.translations('en_US', {
-        SPRACHE: 'pick your language',
-        SPRACHE_DEUTSCH: "german",
-        SPRACHE_AMERICAN_ENGLISCH: "english"
-      });
 
-      $translateProvider.translations('de_DE', {
-        SPRACHE: 'Waehle deine Sprache',
-        SPRACHE_DEUTSCH: "deutsch",
-        SPRACHE_AMERICAN_ENGLISCH: "englisch"
-      });
+    sendData("./public/src/lang/locale-en.json", function(result){
+        $translateProvider.translations('en_US', result);
+    });
+
+    sendData("./public/src/lang/locale-de.json", function(result){
+        $translateProvider.translations('de_DE', result);
+    });
+    
+    $translateProvider.preferredLanguage(getCookie("lang"));
+
 
     $routeProvider
         .when("/", {
@@ -98,20 +112,16 @@ app.config(function($mdThemingProvider, $routeProvider, $locationProvider, $tran
 });
 
 
-app.controller("navigationCtrl", function($scope, $translate){
+app.controller("navigationCtrl", function($scope){
 
     $scope.topics = [
-        { display: "Professoren", link: "/professoren" },
-        { display: "Software", link: "/software" },
-        { display: "Buchempfehlungen", link: "/buch" },
-        { display: "Freizeit / Essen", link: "/freizeit" },
+        { display: "MENU_PROFS", link: "/professoren" },
+        { display: "MENU_SOFTWARE", link: "/software" },
+        { display: "MENU_BOOKS", link: "/buch" },
+        { display: "MENU_FREETIME", link: "/freizeit" },
         { display: "DHBW", link: "/dhbw" },
-        { display: "Link Sammlung", link: "/links" }
+        { display: "MENU_LINKS", link: "/links" }
     ];
-
-    $scope.$watch('language', function() {
-        $translate.use($scope.language);
-      });
 });
 
 
@@ -149,3 +159,37 @@ app.controller("linksCtrl", function($scope, $http){
 
 
 });
+
+
+function sendData(url, callback) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+      xmlhttp = new XMLHttpRequest();
+    } else {
+      // code for older browsers
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        callback(JSON.parse(this.responseText));
+      }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+  }
+
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
